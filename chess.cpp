940de58169ef885003146ec6d5d8cd88fdf58fbc
Luckownia <f.kubala@interia.pl>
2023-07-19 15:31:57 +0200

    Adding Pawn movement function

--- chess.cpp
+++ chess.cpp
@@ -2,10 +2,74 @@
 
 using namespace std;
 
-void view_board()
+string tab[8][8];
+class Pionek{
+public:
+	int x_,y_;
+	Pionek(int x,int y)
+	{
+		x_=x;
+		y_=y;
+	}
+	void move_pionek()
+	{
+		int x,y;
+		bool movable=false;
+		while(movable!=true)
+		{
+			cout<<"Na jaka pozycje X: ";
+			cin>>x;
+			cout<<"Na jaka pozycje Y: ";
+			cin>>y;
+			if(abs(y-y_)==1 || abs(x-x_)) //mozliwe cofanie do naprawy 
+			{
+				if(abs(x-x_)>0)
+				{
+					if(tab[y][x]!="##")
+					{
+						cout<<"Yes";
+						movable=true;
+					}
+					else{
+						cout<<"No";
+					}
+				}
+				else{
+					cout<<"Yes";
+					movable=true;
+				}
+			}
+			else{
+				if(abs(y-y_)==2 && (y==1 || y==7))
+				{
+					if(abs(x-x_)>0)
+					{
+						if(tab[y][x]!="##")
+						{
+							cout<<"Yes";
+							movable=true;
+						}
+						else{
+							cout<<"No";
+						}
+					}
+					else{
+						cout<<"Yes";
+						movable=true;
+					}
+				}
+			}
+			
+		}
+		tab[y][x]=tab[y_][x_];
+		tab[y_][x_]="##";
+	}
+
+};
+
+void board_setup()
 {
     int x,y; //create chessboard tab
-	string tab[8][8];
 	for (y=0;y<8;y++){
 		for(x=0;x<8;x++){
 			tab[y][x] = "##";
@@ -45,21 +109,24 @@ void view_board()
         tab [y][5] = "B1";
         tab [y][6] = "N1";
         tab [y][7] = "R1";
-	
+}
+
+void view_board()
+{
 	for(char word ='A';word<='H';word++)
 	{
 		if(word=='A')
 		{
-			cout<<"  | ";
+			cout<<"Y/X| ";
 		} 
 		cout<<" "<<word<<" | ";
 	}
 	//displaying chessboard 
 	cout<<endl;
 
-    for (y=0;y<8;y++){
-		cout<<y+1<<" | ";
-			for(x=0;x<8;x++){
+    for (int y=0;y<8;y++){
+		cout<<" "<<y+1<<" | ";
+			for(int x=0;x<8;x++){
 				cout << tab[y][x] << " | ";
 			}
 			cout << "\n";
@@ -68,6 +135,10 @@ void view_board()
 
 int main()
 {
-    view_board();
+    board_setup();
+	view_board();
+	Pionek P1(1,1);
+	P1.move_pionek();
+	view_board();
     return 0;
 }
